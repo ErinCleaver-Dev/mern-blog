881117e8ea19848d359e073fb1706046fb6e8339
ecleaver <erincleaver@grcc.email.edu>
2021-10-02 09:39:10 -0400

    Added validation

--- blog-backend/routes/auth.js
+++ blog-backend/routes/auth.js
@@ -2,6 +2,10 @@ const {Router} = require('express');
 const { signup } =require('../controllers/auth')
 const router = Router();
 
-router.post('/signup', signup)
+// validators for signup
+const {runValidation} = require('../validators');
+const {userSingupValidator} =require('../validators/auth');
+
+router.post('/signup',userSingupValidator , runValidation, signup);
 
 module.exports = router;
--- blog-backend/validators/auth.js
+++ blog-backend/validators/auth.js
@@ -1,6 +1,16 @@
 const {check} = require('express-validator')
 
-
 module.exports  = {
-
+    userSingupValidator: [
+        check('name')
+            .not()
+            .isEmpty()
+            .withMessage('Name is Required'),
+        check('email')
+            .isEmail()
+            .withMessage('Must be a validd email address'),
+        check('password')
+            .isLength({min: 8, max: 24})
+            .withMessage('Password must be at least 8 characters and no more')
+    ]
 }
